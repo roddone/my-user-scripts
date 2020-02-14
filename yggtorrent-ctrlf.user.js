e7df0d346a7b2423a86f950f9429b6f355023521
Romain ODDONE <34140141+roddone@users.noreply.github.com>
2020-02-14 17:45:39 +0100

    Update yggtorrent-ctrlf.user.js
    update include URL regex to match with www, www2 or nothing

--- yggtorrent-ctrlf.user.js
+++ yggtorrent-ctrlf.user.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  allows to ctrl+f to search on yggtorrent
 // @author       Romain Oddone
-// @include      /^https?://www2\.yggtorrent\.(ws|se)*/
+// @include      /^https?://(www2|www)?\.yggtorrent\.(ws|se)*/
 // @grant        none.
 // @run-at       document-ready
 // @updateURL   https://github.com/roddone/my-user-scripts/raw/master/yggtorrent-ctrlf.user.js
