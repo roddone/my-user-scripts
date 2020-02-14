0b63f47f7ec569a0cdf5128b2344ef076de24fc6
Romain ODDONE <34140141+roddone@users.noreply.github.com>
2020-02-14 17:43:10 +0100

    Create yggtorrent-ctrlf.user.js

---
+++ yggtorrent-ctrlf.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         add keyboard search
+// @namespace    YggTorrent
+// @version      0.2
+// @description  allows to ctrl+f to search on yggtorrent
+// @author       Romain Oddone
+// @include      /^https?://www2\.yggtorrent\.(ws|se)*/
+// @grant        none.
+// @run-at       document-ready
+// @updateURL   https://github.com/roddone/my-user-scripts/raw/master/yggtorrent-ctrlf.user.js
+// @downloadURL https://github.com/roddone/my-user-scripts/raw/master/yggtorrent-ctrlf.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.onkeydown = function(evt){
+        if(evt.key === 'f' && evt.ctrlKey === true){
+            evt.preventDefault();
+            document.querySelector('body > header > div > form > div > input[type=text]').focus()
+        }
+    };
+})();
