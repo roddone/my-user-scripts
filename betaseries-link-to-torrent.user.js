28a46cbd706b4c6a8ec5bb52960505243da9c695
romain oddone <romain.oddone@outlook.com>
2021-01-30 15:59:17 +0100

    wait for episodes to be displayed

--- betaseries-link-to-torrent.user.js
+++ betaseries-link-to-torrent.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         add yggtorrent link
 // @namespace    Betaseries
-// @version      0.1
+// @version      0.2
 // @description  add link to yggtorrent search in betaseries episodes list
 // @author       Romain Oddone
 // @include      https://www.betaseries.com/membre/seddhaz/episodes
@@ -11,8 +11,11 @@
 // @downloadURL https://github.com/roddone/my-user-scripts/raw/master/betaseries-link-to-torrent.user.js
 // ==/UserScript==
 
-(function () {
+(async function () {
     'use strict';
+
+    await waitForResults();
+    // console.log('found !')
     document.querySelectorAll('#episodes_container .episode').forEach(episode_elt => {
         let title = episode_elt.querySelector('.ep').innerText
         let episode = episode_elt.querySelector('a:nth-child(2)').innerText
@@ -25,5 +28,20 @@
         child.innerText = 'YggTorrent'
         episode_elt.querySelector('.episode-side').appendChild(child)
     });
+
+    async function waitForResults(){
+        return new Promise((resolve, reject) => {
+            setTimeout(async () => {
+                if(!document.querySelector('#episodes_container .episodes')){
+                    // console.log('waiting a little more ...')
+                    await waitForResults();
+                    resolve();
+                }
+                else{
+                    resolve();
+                }
+            }, 200);
+        });
+    }
 
 })();
