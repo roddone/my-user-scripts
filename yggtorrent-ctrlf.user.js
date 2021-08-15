b5b1261301aedf77a47d9011ac3c45016c8e7861
Romain ODDONE <34140141+roddone@users.noreply.github.com>
2021-08-15 10:18:21 +0200

    add new yggtorrent extension

--- yggtorrent-ctrlf.user.js
+++ yggtorrent-ctrlf.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         add keyboard search
 // @namespace    YggTorrent
-// @version      0.7
+// @version      0.8
 // @description  allows to ctrl+f to search on yggtorrent
 // @author       Romain Oddone
-// @include      /^https?://(www4\.|www3\.|www2\.|www\.)?yggtorrent\.(ws|se|si|li)*/
+// @include      /^https?://(www4\.|www3\.|www2\.|www\.)?yggtorrent\.(ws|se|si|li|nz)*/
 // @grant        none.
 // @run-at       document-ready
 // @updateURL   https://github.com/roddone/my-user-scripts/raw/master/yggtorrent-ctrlf.user.js
