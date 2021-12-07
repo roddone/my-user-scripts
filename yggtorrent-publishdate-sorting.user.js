f266917e9267fca88d64bd4b7397df7124f4b7e9
Romain ODDONE <34140141+roddone@users.noreply.github.com>
2021-12-07 19:03:58 +0100

    new yggtorrent domain extension

--- yggtorrent-publishdate-sorting.user.js
+++ yggtorrent-publishdate-sorting.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         automatic publish date sorting
 // @namespace    YggTorrent
-// @version      0.7
+// @version      0.8
 // @description  Automatically sorts search results by descending publication date
 // @author       Romain Oddone
-// @include      /^https?://(www4\.|www3\.|www2\.|www\.)?yggtorrent\.(ws|se|si|li|nz)/engine/search*/
+// @include      /^https?://(www4\.|www3\.|www2\.|www\.)?yggtorrent\.(ws|se|si|li|nz|re)/engine/search*/
 // @grant        none.
 // @run-at       document-start
 // @updateURL   https://github.com/roddone/my-user-scripts/raw/master/yggtorrent-publishdate-sorting.user.js
