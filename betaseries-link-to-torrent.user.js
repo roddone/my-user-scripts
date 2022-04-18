cb2c3da50d307056a3c865c008d39da03a8b0962
Romain ODDONE <34140141+roddone@users.noreply.github.com>
2022-04-18 10:39:30 +0200

    Update betaseries-link-to-torrent.user.js

--- betaseries-link-to-torrent.user.js
+++ betaseries-link-to-torrent.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         add yggtorrent link
 // @namespace    Betaseries
-// @version      0.5
+// @version      0.6
 // @description  add link to yggtorrent search in betaseries episodes list
 // @author       Romain Oddone
 // @include      https://www.betaseries.com/membre/seddhaz/episodes
@@ -21,7 +21,7 @@
         let episode = episode_elt.querySelector('a:nth-child(2)').innerText
         let child = document.createElement('a');
 
-        child.href = `https://www3.yggtorrent.nz/engine/search?name=${title}+${episode}&do=search`
+        child.href = `https://www5.yggtorrent.la/engine/search?name=${title}+${episode}&do=search`
         child.target = '_blank'
         child.classList.add('btn-dl'); child.classList.add('btn-reset')
         child.style = 'width:unset;'
