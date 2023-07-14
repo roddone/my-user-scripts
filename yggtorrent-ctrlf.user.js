b204733acce85b597cb29ef8edd0b2cf623edd0a
Romain ODDONE <34140141+roddone@users.noreply.github.com>
2023-07-14 10:25:27 +0200

    change yggtorrent domain to wtf

--- yggtorrent-ctrlf.user.js
+++ yggtorrent-ctrlf.user.js
@@ -4,7 +4,7 @@
 // @version      0.13
 // @description  allows to ctrl+f to search on yggtorrent
 // @author       Romain Oddone
-// @include      /^https?://(www6\.|www5\.|www4\.|www3\.|www2\.|www\.)?yggtorrent\.(do|lol|ws|se|si|li|nz|re|la|fi)*/
+// @include      /^https?://(www6\.|www5\.|www4\.|www3\.|www2\.|www\.)?yggtorrent\.(wtf|do|lol|ws|se|si|li|nz|re|la|fi)*/
 // @grant        none.
 // @run-at       document-ready
 // @updateURL   https://github.com/roddone/my-user-scripts/raw/master/yggtorrent-ctrlf.user.js
