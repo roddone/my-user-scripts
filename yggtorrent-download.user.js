b8030db5a1a6aa5083c6274232500dcc967f147a
Romain Oddone <34140141+roddone@users.noreply.github.com>
2024-05-03 12:20:31 +0200

    new yggtorrent domain

--- yggtorrent-download.user.js
+++ yggtorrent-download.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         allows to download with keyboard action
 // @namespace    YggTorrent
-// @version      0.16
+// @version      0.17
 // @description  allows to ctrl+f to search on yggtorrent
 // @author       Romain Oddone
-// @include      /^https?://(www6\.|www5\.|www4\.|www3\.|www2\.|www\.)?yggtorrent\.(qa|wtf|do|lol|ws|se|si|li|nz|re|la|fi)/torrent*/
+// @include      /^https?://(www6\.|www5\.|www4\.|www3\.|www2\.|www\.)?yggtorrent\.(cool|qa|wtf|do|lol|ws|se|si|li|nz|re|la|fi)/torrent*/
 // @grant        none.
 // @run-at       document-ready
 // @updateURL   https://github.com/roddone/my-user-scripts/raw/master/yggtorrent-download.user.js
--- yggtorrent-publishdate-sorting.user.js
+++ yggtorrent-publishdate-sorting.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         automatic publish date sorting
 // @namespace    YggTorrent
-// @version      0.16
+// @version      0.17
 // @description  Automatically sorts search results by descending publication date
 // @author       Romain Oddone
-// @include      /^https?://(www6\.|www5\.|www4\.|www3\.|www2\.|www\.)?yggtorrent\.(qa|wtf|do|lol|ws|se|si|li|nz|re|la|fi)/engine/search*/
+// @include      /^https?://(www6\.|www5\.|www4\.|www3\.|www2\.|www\.)?yggtorrent\.(cool|qa|wtf|do|lol|ws|se|si|li|nz|re|la|fi)/engine/search*/
 // @grant        none.
 // @run-at       document-start
 // @updateURL   https://github.com/roddone/my-user-scripts/raw/master/yggtorrent-publishdate-sorting.user.js
