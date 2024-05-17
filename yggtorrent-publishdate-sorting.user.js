c3942d77fb44b4bbb0b9bc40b22e560e31aacb01
Romain Oddone <34140141+roddone@users.noreply.github.com>
2024-05-17 16:13:04 +0200

    update yggtorrent urls for ygg.re

--- yggtorrent-publishdate-sorting.user.js
+++ yggtorrent-publishdate-sorting.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         automatic publish date sorting
 // @namespace    YggTorrent
-// @version      0.18
+// @version      0.19
 // @description  Automatically sorts search results by descending publication date
 // @author       Romain Oddone
 // @include      /^https?://(www6\.|www5\.|www4\.|www3\.|www2\.|www\.)?yggtorrent\.(cool|qa|wtf|do|lol|ws|se|si|li|nz|re|la|fi)/engine/search*/
-// @include      /^https?://ygg\.(re)*/
+// @include      /^https?://ygg\.(re)/engine/search*/
 // @grant        none.
 // @run-at       document-start
 // @updateURL   https://github.com/roddone/my-user-scripts/raw/master/yggtorrent-publishdate-sorting.user.js
