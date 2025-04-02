85dfd44afdff9a562e21e1a0298308d613a14613
Romain Oddone <34140141+roddone@users.noreply.github.com>
2025-04-02 22:17:52 +0200

    update versions

--- yggtorrent-download.user.js
+++ yggtorrent-download.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         allows to download with keyboard action
 // @namespace    YggTorrent
-// @version      0.20
+// @version      0.21
 // @description  allows to ctrl+f to search on yggtorrent
 // @author       Romain Oddone
 // @include      /^https?://(www6\.|www5\.|www4\.|www3\.|www2\.|www\.)?yggtorrent\.(cool|qa|wtf|do|lol|ws|se|si|li|nz|re|la|fi|top)/torrent*/
